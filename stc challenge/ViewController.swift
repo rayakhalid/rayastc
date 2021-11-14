9a5772d74fedd1bc82b90f52fdb3eb990e2b69df
raya <reyakh.h@gmail.com>
2021-11-14 19:33:42 +0300

    Initial Commit

---
+++ stc challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stc challenge
+//
+//  Created by Raya Khalid on 14/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
